4ac4cb947ecd2577cbf33542fcc5f00201885f9e
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-11-25 20:41:38 +0600

    search weather with input

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import './App.css'
 import Home from './Pages/Home/Home'
 
 function App() {
-
-
-
   return (
 
      <div>
--- src/Pages/Home/SearchWeather.jsx
+++ src/Pages/Home/SearchWeather.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
 import { getWeatherData } from '../../WeatherServices/weatherServices';
-
+import hotBg from '../../assets/hot.jpg';
+import coldBg from '../../assets/cold.jpg';
+import './SearchWeather.css';
+import Description from './Description';
 const SearchWeather = () => {
   useEffect(() => {
     const getWeatherAllData = async () => {
@@ -10,8 +13,27 @@ const SearchWeather = () => {
     getWeatherAllData();
   }, []);
   return (
-    <div>
-      <input type="text" />
+    <div className='app' style={{backgroundImage: `url(${hotBg})`}}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section_inputs">
+          <input type="text" name='city' placeholder="Enter City.."/>
+          <button>°F</button>
+          </div>
+          <div className="section section_temperature">
+          <div className="icon">
+            <h2>Dhaka, BG</h2>
+            <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="WeatherIcon" />
+            <h2>Cloudy</h2>
+          </div>
+          <div className="temperature">
+            <h1>32 °C</h1>
+          </div>
+          </div>
+            {/* bottom description */}
+          <Description/>
+        </div>
+      </div>
     </div>
   );
 };
